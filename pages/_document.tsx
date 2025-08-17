dd49826ab471f567f4b8cad03455a48c095c3982
Your Name <your_email@example.com>
2025-08-17 21:30:51 +0530

    commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="RATING" content="RTA-5042-1996-1400-1577-RTA" />
+      </Head>
       <body>
         <Main />
         <NextScript />
